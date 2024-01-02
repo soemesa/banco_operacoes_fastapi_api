25ee385014e0174c7d000abdca7dd2c72a1cdce7
Soilenyis <soilenyis@gmail.com>
2024-01-02 13:32:18 -0300

    criando camada models e database

---
+++ src/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, DateTime, Float
+
+from database import Base
+
+
+class Conta(Base):
+    __tablename__ = "conta"
+
+    numero_de_conta: int = Column(Integer, primary_key=True, index=True, nullable=False)
+    created_at: datetime = Column(DateTime, nullable=False)
+    valor: float = Column(Float, nullable=False)
+
+
