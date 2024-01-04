8d5707fd158354e8f6869be2cdaba2fd36630590
Soilenyis <soilenyis@gmail.com>
2024-01-04 14:10:10 -0300

    usando exception para valor unico no banco de dados

--- src/rotas.py
+++ src/rotas.py
@@ -2,6 +2,8 @@ import logging
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
+from sqlalchemy.dialects.postgresql import psycopg2
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 from starlette.responses import Response
 
@@ -22,30 +24,24 @@ def welcome() -> str:
 # def read_transacoes():
 #     return {'transacoes': transacoes}
 #
-
-
+#
+# db_transacao = session.scalar(
+#             select(Conta).where(Conta.numero_de_conta == transacao.numero_de_conta)
+#         )
 
 @router.post('/transacoes', response_model=TransacaoSalida, status_code=201)
 def create_transacao(response: Response, transacao: TransacaoEntrada, session: Session = Depends(get_session)):
     try:
-        db_transacao = session.scalar(
-            select(Conta).where(Conta.numero_de_conta == transacao.numero_de_conta)
-        )
-
-        if db_transacao:
-            response.status_code = 400
-            raise HTTPException(
-                status_code=400, detail='Número de conta ja existe'
-            )
-
         db_transacao = Conta(
             numero_de_conta=transacao.numero_de_conta, valor=transacao.valor
         )
         session.add(db_transacao)
         session.commit()
         session.refresh(db_transacao)
+    except IntegrityError:
+        return TransacaoSalida(status='error', message='Número de conta existente')
     except Exception as err:
         return TransacaoSalida(status='error', message=f'{err}')
 
 
-    return TransacaoSalida(status='created', message='O registro foi criado com sucesso!')
+    return TransacaoSalida(status='created', message='Número de conta adicionado com sucesso!')
